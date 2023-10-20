98acae728f33d56eac369c28ba618308788a790c
Blackpossum <yosaphatharwindra@gmail.com>
2023-10-20 21:08:19 +0700

    update ui & script

--- sript.js
+++ sript.js
@@ -1,6 +1,15 @@
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $("#mainNavbar");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-    });
-});
+const navbar = document.getElementById("mainNavbar");
+function makeNavbarSticky() {
+  if (window.scrollY > 0) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+window.addEventListener("scroll", makeNavbarSticky);
+
+
+
+
+
